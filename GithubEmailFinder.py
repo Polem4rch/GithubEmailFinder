72d099f14ee27cab177f35b21385dd84ca2cc222
NullWin <44825899+NullWin@users.noreply.github.com>
2020-10-24 04:14:39 -0300

    Create GithubEmailFinder.py

---
+++ GithubEmailFinder.py
@@ -0,0 +1,11 @@
+import requests
+
+
+url1 = "https://api.github.com/users/"
+url2 = input("add username ")
+url3 = "/events/public"
+all = url1 + url2 + url3
+
+
+response = requests.get(all)
+print(response.json())
