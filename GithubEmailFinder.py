5e347b9d76d6aba3ac5c60bbb9cdc1d29c20dc00
prykon <dariomanoukian@gmail.com>
2020-10-25 13:18:15 -0300

    made email search recursive for all elements

--- GithubEmailFinder.py
+++ GithubEmailFinder.py
@@ -6,5 +6,10 @@ url = "https://api.github.com/users/%s/events/public" % username
 
 response = requests.get(url)
 
-data = json.loads(response.content.decode('utf-8'))[0]['payload']['commits'][0]['author']['email']
-print(data)
+data = json.loads(response.content.decode('utf-8'))
+
+for d in data:
+	try:
+		print(d['payload']['commits'][0]['author']['email'])
+	except:
+		pass
