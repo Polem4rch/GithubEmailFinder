dfd6cc200dbafc1d038367e468fc38cb39324f4f
prykon <dariomanoukian@gmail.com>
2020-10-25 12:42:05 -0300

    tested locally due to api access restrictions

--- GithubEmailFinder.py
+++ GithubEmailFinder.py
@@ -1,25 +1,10 @@
 import requests, json
 
+
 username = input("add username: ")
 url = "https://api.github.com/users/%s/events/public" % username
 
-
 response = requests.get(url)
-result = json.loads(response.content.decode('utf-8'))
 
-user_list = []
-
-for r in result:
-	for k,v in iter(r.items()):
-		if k == 'actor':
-			if v not in user_list:
-				user_list.append(v)
-
-print()
-for u in user_list:
-	for k,v in u.items():
-		print('%s: %s'%(k.upper(),v))
-	print()
-
-if not user_list:
-	print('error: user not found')
+data = json.loads(response.content.decode('utf-8'))[0]['payload']['commits'][0]['author']['email']
+print(data)
